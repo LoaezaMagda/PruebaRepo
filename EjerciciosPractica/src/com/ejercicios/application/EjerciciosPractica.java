b27e3ba23a9e0d25a315c6061b5d7de62829daa1
LoaezaMagda <loaeza.magdalena@gmail.com>
2019-07-25 13:21:29 -0500

    mucho miau

---
+++ EjerciciosPractica/src/com/ejercicios/application/EjerciciosPractica.java
@@ -0,0 +1,35 @@
+package com.ejercicios.application;
+
+public class EjerciciosPractica {
+
+	public static void main(String[] args) {
+		
+	int numero=5;
+	int potencia=2;
+	 int calculo = 1;
+	for (int x=0;x<potencia;x++)
+	{
+		 calculo *=numero;
+		System.out.println(calculo);
+	}
+	
+	System.out.println(2+6<<1);//suma el mismo numero las veces que especicifque el numero<<veces asumar
+	
+	System.out.println(17>>2);//
+	
+	 int x = 2;
+     int y = 2;						
+     boolean b;
+     //System.out.println(b);
+     
+     if(( x != 0 ) && ( y != 0 ))
+     {
+    	 System.out.println("Hola");
+    	 
+     }
+   
+	
+	}
+
+
+}
